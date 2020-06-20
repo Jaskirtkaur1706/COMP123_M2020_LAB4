f6b6fa0e78bcd59e9334367696869a6cad3aaa80
JASKIRAT KAUR <jaskiratkaur1706@gmail.com>
2020-06-19 22:26:13 -0500

    Add project files.

---
+++ COMP123_M2020_LAB4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_LAB4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person jaskirat = new Person("Jaskirat", age: 20);
+
+            jaskirat.SaysHello();
+
+            Console.ReadLine();
+        }
+    }
+}
